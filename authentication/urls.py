22d4d545498b1b3a99b6f220a5c4158b60b0c0d4
sainikitha99 <sainikitha.dmm@gmail.com>
2018-02-17 07:06:42 +0530

    added registration and login forms

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,4 +6,5 @@ AUTH_URLS = [
 	url(r'^user_registration/', user_registration, name='user_registrations'),
 	url(r'^get_requests/', get_user_requests, name='user_requests'),
 	url(r'^main_page/', main_page, name='main_page'),
+	url(r'^hospital_registration/', hospital_registration, name='hospital_registrations'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -10,11 +10,17 @@ def get_user_requests(request):
 	requests = UserRequest.objects.filter(user_id=request.user.id)
 
 
-def user_registration(request):
-	requests = UserRequest.objects.all()
-	context = {"requests": requests}
-	return render(request, 'user_reg.html', context)
 def main_page(request):
 	requests = UserRequest.objects.all()
 	context = {"requests": requests}
 	return render(request, 'main_page.html', context)
+
+def user_registration(request):
+	requests = UserRequest.objects.all()
+	context = {"requests": requests}
+	return render(request, 'user_reg.html', context)
+
+def hospital_registration(request):
+	requests = UserRequest.objects.all()
+	context = {"requests": requests}
+	return render(request, 'hospital_reg.html', context)
