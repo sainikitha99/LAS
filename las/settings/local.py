c6f50ae75482749a7867ac7013700e992170d289
sainikitha99 <sainikitha.dmm@gmail.com>
2018-02-21 07:28:33 +0530

    added local settings

--- las/settings/local.py
+++ las/settings/local.py
@@ -1,1 +1,3 @@
 from .base import *
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY ='548461450482-klqkgdb1o9ujib9k2qonrlvsm7fubhpc.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'CCWG8pbyVDyDXjpc0btqHQ1P'
