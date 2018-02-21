3ea8bd73c9a19a99f289d2f056d633efc203b207
sainikitha99 <sainikitha.dmm@gmail.com>
2018-02-21 07:19:21 +0530

    removed auth-keys

--- las/settings/base.py
+++ las/settings/base.py
@@ -139,5 +139,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY ='548461450482-klqkgdb1o9ujib9k2qonrlvsm7fubhpc.apps.googleusercontent.com'
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'CCWG8pbyVDyDXjpc0btqHQ1P'
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY =''
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = ''
