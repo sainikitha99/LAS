d0cd606ed2d9696c7600d30be762acdc30fff772
Yashwanth <yashwanthbabu.gujarathi@gmail.com>
2018-04-07 18:14:51 +0530

    Fix stylings and map zoom level

--- core/static/js/load_map.js
+++ core/static/js/load_map.js
@@ -6,7 +6,7 @@
  function initMap() {
    map = new google.maps.Map(document.getElementById('map'), {
      center: {lat: -34.397, lng: 150.644},
-     zoom: 12
+     zoom: 14
    });
    infoWindow = new google.maps.InfoWindow;
    $("#latitude").val("-34.397");
